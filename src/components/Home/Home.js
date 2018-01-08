8e56d39fab1b3d23b4fa82e3964ab6b463b3fd58
CalderMarshall <cmarshall72@gmail.com>
2018-01-08 14:40:10 -0500

    adds app navigator, installs react-native-router-flux, also installs native-base

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View, Image, StyleSheet, StatusBar } from "react-native";
+
+export default class Home extends Component {
+    render () {
+        return (
+            <View>
+                <Text style={{fontSize: 30, marginTop: 80}}>HOME</Text>
+            </View>
+        )
+
+    }
+
+}
