addef6057765feaffa73022ee10577bb9ceef6dc
mcguiretj <mcguiretj@goldmail.etsu.edu>
2018-01-10 21:39:34 -0500

    install dependencies and update App for routing

--- App.js
+++ App.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-// import SearchPage from './src/components/SearchPage/SearchPage';
-// import ProductDetails from './src/components/ProductDetails/ProductDetails';
-// import FilterSearch from './src/components/FilterSearch/FilterSearch';
-// import HomeImages from './src/components/HomeImages/HomeImages';
+import SearchPage from './src/components/SearchPage/SearchPage';
+import ProductDetails from './src/components/ProductDetails/ProductDetails';
+import FilterSearch from './src/components/FilterSearch/FilterSearch';
+import HomeImages from './src/components/HomeImages/HomeImages';
 
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         {/* <SearchPage /> */}
-//         {/* <ProductDetails /> */}
-//         {/* <FilterSearch /> */}
-//         {/* <HomeImages /> */}
-//       </View>
-
-import Router from './src/Router'
-import Home from './src/components/Home/Home'
 export default class App extends React.Component {
   render() {
     return (
-      <Router />
-    );
+      <View style={styles.container}>
+        <SearchPage />
+        {/* <ProductDetails /> */}
+        {/* <FilterSearch /> */}
+        {/* <HomeImages /> */}
+      </View>
+    )
   }
 }
+
+// import Router from './src/Router'
+// import Home from './src/components/Home/Home'
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <Router />
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
