1f5e6b41e91e150c81d7a8ea1a2065f98ba42454
mcguiretj <mcguiretj@goldmail.etsu.edu>
2018-01-11 10:53:52 -0500

    clean up code and use routing

--- App.js
+++ App.js
@@ -1,24 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import SearchPage from './src/components/SearchPage/SearchPage';
-import ProductDetails from './src/components/ProductDetails/ProductDetails';
-import FilterSearch from './src/components/FilterSearch/FilterSearch';
-import HomeImages from './src/components/HomeImages/HomeImages';
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-        {/* <SearchPage /> */}
-        {/* <ProductDetails /> */}
-        {/* <FilterSearch /> */}
-        {/* <HomeImages /> */}
-      {/* </View>
-    )
-  }
-} */}
-
 import Router from './src/Router'
 import Home from './src/components/Home/Home'
 export default class App extends React.Component {
@@ -28,13 +10,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 50,
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
